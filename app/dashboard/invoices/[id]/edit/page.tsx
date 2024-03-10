09c7491826c801a0a6a13ed3a76dd7497e58f0e7
He Jia Min <he-jiamin@foxmail.com>
2024-03-10 16:52:01 +0800

    feat 提高可访问性

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,9 +1,9 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import {fetchInvoiceById, fetchCustomers} from '@/app/lib/data';
-import {notFound} from "next/navigation";
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
-export default async function Page({params}: { params: { id: string } }) {
+export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
@@ -11,14 +11,14 @@ export default async function Page({params}: { params: { id: string } }) {
   ]);
 
   if (!invoice) {
-    notFound()
+    notFound();
   }
 
   return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          {label: 'Invoices', href: '/dashboard/invoices'},
+          { label: 'Invoices', href: '/dashboard/invoices' },
           {
             label: 'Edit Invoice',
             href: `/dashboard/invoices/${id}/edit`,
@@ -26,7 +26,7 @@ export default async function Page({params}: { params: { id: string } }) {
           },
         ]}
       />
-      <Form invoice={invoice} customers={customers}/>
+      <Form invoice={invoice} customers={customers} />
     </main>
   );
 }
